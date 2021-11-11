0de94950198fb42277cd0a43caf563bdf4ea30ed
rahiii <rahmaniali1436@gmail.com>
2021-11-11 17:05:27 +0330

    pegue

---
+++ oop.py
@@ -0,0 +1,24 @@
+class pegue:
+    count = 0
+
+    def __init__(self,type , color ='white',ring='mamuli',airbag='nadare'):
+        self.type =type
+        self.color = color
+        self.ring = ring
+        self.airbag = airbag
+        
+    def printcolor(self):
+        print(self.color)
+        print(self.type)
+        
+p1 = pegue('tak','red')
+
+p1.printcolor()
+
+
+
+
+
+
+
+
